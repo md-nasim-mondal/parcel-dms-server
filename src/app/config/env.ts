9c3e157fe9ecf300fdac5a237f0611bdf8c6c801
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-09-09 19:41:15 +0600

    initial basic server project setup

---
+++ src/app/config/env.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface EnvConfig {
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: string;
+  FRONTEND_URL: string;
+}
+
+export const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariables: string[] = [
+    "PORT",
+    "DB_URL",
+    "NODE_ENV",
+    "FRONTEND_URL",
+  ];
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing require environment variabl ${key}`);
+    }
+  });
+
+  return {
+    PORT: process.env.PORT as string,
+    DB_URL: process.env.DB_URL as string,
+    NODE_ENV: process.env.NODE_ENV as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+  };
+};
+
+export const envVars = loadEnvVariables();
