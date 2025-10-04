7fb47e2a82d15ec91ce4a7bffadefadd1dd305d8
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-10-04 17:02:33 +0600

    update query builder

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -1,5 +1,5 @@
 import { Query } from "mongoose";
-import { excludeField } from "../constants";
+import { excludeField } from "../constants"; 
 
 export class QueryBuilder<T> {
   public modelQuery: Query<T[], T>;
@@ -18,43 +18,52 @@ export class QueryBuilder<T> {
       delete filter[field];
     }
 
-    this.modelQuery = this.modelQuery.find(filter); // Tour.find().find(filter)
+    // FIX 1: খালি মানযুক্ত ফিল্ডগুলো বাদ দিন
+    Object.keys(filter).forEach((key) => {
+      if (!filter[key]) {
+        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+        delete filter[key];
+      }
+    });
+
+    this.modelQuery = this.modelQuery.find(filter);
 
     return this;
   }
 
   search(searchableField: string[]): this {
-    const searchTerm = this.query.searchTerm || "";
-    const searchQuery = {
-      $or: searchableField.map((field) => ({
-        [field]: { $regex: searchTerm, $options: "i" },
-      })),
-    };
-    this.modelQuery = this.modelQuery.find(searchQuery);
+    const searchTerm = this.query.searchTerm;
+
+    // FIX 2: কেবল searchTerm থাকলে কুয়েরি চালান
+    if (searchTerm) {
+      const searchQuery = {
+        $or: searchableField.map((field) => ({
+          [field]: { $regex: searchTerm, $options: "i" },
+        })),
+      };
+      this.modelQuery = this.modelQuery.find(searchQuery);
+    }
+
     return this;
   }
 
   sort(): this {
     const sort = this.query.sort || "-createdAt";
-
     this.modelQuery = this.modelQuery.sort(sort);
-
     return this;
   }
+
   fields(): this {
     const fields = this.query.fields?.split(",").join(" ") || "";
-
     this.modelQuery = this.modelQuery.select(fields);
-
     return this;
   }
+
   paginate(): this {
     const page = Number(this.query.page) || 1;
     const limit = Number(this.query.limit) || 10;
     const skip = (page - 1) * limit;
-
     this.modelQuery = this.modelQuery.skip(skip).limit(limit);
-
     return this;
   }
 
@@ -64,11 +73,11 @@ export class QueryBuilder<T> {
 
   async getMeta() {
     const filterQuery = this.modelQuery.getFilter();
-    const totalDocuments = await this.modelQuery.model.countDocuments(filterQuery);
-
+    const totalDocuments = await this.modelQuery.model.countDocuments(
+      filterQuery
+    );
     const page = Number(this.query.page) || 1;
     const limit = Number(this.query.limit) || 10;
-
     const totalPage = Math.ceil(totalDocuments / limit);
 
     return { page, limit, total: totalDocuments, totalPage };
